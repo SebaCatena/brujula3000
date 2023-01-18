3d2768cdda897f8a542b078c75ba2aa74d308416
SebaCatena <catenaccisebastian@gmail.com>
2023-01-18 18:44:21 -0300

    comienzo del proyecto

---
+++ principal.php
@@ -0,0 +1,38 @@
+<html>
+	<head>
+		<title>Pagina principal</title>
+		<link rel="stylesheet" type="text/css" href="estilos.css" media="screen" />
+	</head>
+	<body>
+		<div id="header">
+			<ul class="nav">
+				<li><a href="">Empleados</a>
+						<ul>
+						<li><a href="altaempleado.php">Agregar</a></li>
+						<li><a href="">Modificar</a></li>
+						<li><a href="">Eliminar</a></li>
+				</li>
+	</ul>
+				</li>
+				<li><a href="">Sucursales</a>
+					<ul>
+						<li><a href="altasucursal.php">Agregar</a></li>
+						<li><a href="">Modificar</a></li>
+						<li><a href="">Eliminar</a></li>
+							
+						</li>
+					</ul>
+				</li>
+				<li><a href="">Areas</a>
+					<ul>
+						<li><a href="">Submenu1</a></li>
+						<li><a href="">Submenu2</a></li>
+						<li><a href="">Submenu3</a></li>
+						<li><a href="">Submenu4</a></li>
+					</ul>
+				</li>
+				<li><a href="">Contacto</a></li>
+			</ul>
+		</div>
+	</body>
+</html>
